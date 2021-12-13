98e6ed6e641a94b3933e99738400f35eb024fd7c
FedozZz <titov.bigf@gmail.com>
2021-12-13 11:23:50 +0300

    add svm soft margin.

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ cvxopt.solvers.options['show_progress'] = False
 
 
 def get_separable_data():
-    return datasets.make_blobs(n_samples=100, centers=2, n_features=2, cluster_std=1.25, random_state=9)
+    x, y = datasets.make_blobs(n_samples=100, centers=2, n_features=2, cluster_std=1.25, random_state=9)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x = min_max_scaler.fit_transform(x)
+    y[y == 0] = -1
+    return x, y
+
+
+def get_inseparable_data():
+    x, y = datasets.make_blobs(n_samples=100, centers=2, n_features=2, cluster_std=3.25, random_state=9)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x = min_max_scaler.fit_transform(x)
+    y[y == 0] = -1
+    return x, y
 
 
 class SVMHardMargin:
@@ -48,6 +60,44 @@ class SVMHardMargin:
         return [np.sign(np.dot(self.w, xt[i]) + self.b) for i in range(len(xt))]
 
 
+class SVMSoftMargin:
+    def __init__(self, c):
+        self.w = None
+        self.margin = None
+        self.b = None
+        self.c = c
+        self.support_vectors = None
+
+    def train(self, x, y):
+        x_shape = len(x)
+        P = cvxopt.matrix([[y[i] * y[j] * np.dot(x[i, :], x[j, :]) for j in range(x_shape)] for i in range(x_shape)])
+        q = cvxopt.matrix(-np.ones(x_shape))
+        A = cvxopt.matrix(np.array(y), (1, x_shape), 'd')
+        b = cvxopt.matrix(np.zeros(1))
+        G = cvxopt.matrix(np.vstack((-np.eye(x_shape), np.eye(x_shape))))
+        h = cvxopt.matrix(np.vstack(np.hstack((np.zeros(x_shape), np.ones(x_shape) * self.c))))
+        sol = cvxopt.solvers.qp(P, q, G, h, A, b)
+        alpha = np.array(sol['x'])
+        self.w = np.array([sum(alpha[i] * y[i] * x[i, 0] for i in range(x_shape)),
+                           sum(alpha[i] * y[i] * x[i, 1] for i in range(x_shape))]).flatten()
+        print('self.w', self.w)
+        self.margin = 2 / np.linalg.norm(self.w)
+        print('self.margin', self.margin)
+        sv_mask = ((1e-3 < alpha) & (alpha < self.c)).flatten()
+        self.support_vectors = x[sv_mask]
+        print('self.support_vectors', self.support_vectors)
+        sv_y = y[sv_mask]
+        self.b = np.mean(
+            [sv_y[i] - np.dot(self.support_vectors[i], self.w) for i in range(len(self.support_vectors))])
+        print('self.b', self.b)
+
+    def predict(self, xt):
+        return np.array([np.dot(self.w, xt[i]) + self.b for i in range(len(xt))])
+
+    def predict_sign(self, xt):
+        return [np.sign(np.dot(self.w, xt[i]) + self.b) for i in range(len(xt))]
+
+
 def plot_graph(x, y, model):
     ax = plt.gca()
     plt.scatter(x[:, 0], x[:, 1], c=y, s=50, cmap='winter')
@@ -70,11 +120,10 @@ def plot_graph(x, y, model):
 
 svm_hard = SVMHardMargin()
 x, y = get_separable_data()
-
-min_max_scaler = preprocessing.MinMaxScaler()
-x = min_max_scaler.fit_transform(x)
-y[y == 0] = -1
-
 svm_hard.train(x, y)
-
 plot_graph(x, y, svm_hard)
+
+svm_soft = SVMSoftMargin(2)
+x, y = get_inseparable_data()
+svm_soft.train(x, y)
+plot_graph(x, y, svm_soft)
